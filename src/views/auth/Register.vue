d3512784285cc78a79640c3667be861d32b205fe
ducnt1003 <nguyentrungduc0310@gmail.com>
2022-11-27 01:25:47 +0700

    create login page

---
+++ src/views/auth/Register.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+    <CContainer>
+      <CRow class="justify-content-center">
+        <CCol xs="12" md="10" lg="8" xl="6">
+          <CCard class="p-4">
+            <CCardBody>
+              <h1 class="text-center">REGISTER</h1>
+              <Form
+                as="CForm"
+                v-slot="{ errors }"
+                @submit="onSubmit"
+                :validation-schema="schema"
+              >
+                <CFormFloating class="my-4">
+                  <Field
+                    as="CFormInput"
+                    name="username"
+                    type="text"
+                    id="username"
+                    placeholder="username"
+                    :invalid="!!errors?.username"
+                    :feedback-invalid="errors?.username"
+                  />
+                  <CFormLabel for="username">Username </CFormLabel>
+                </CFormFloating>
+
+                <CFormFloating class="my-4">
+                  <Field
+                    as="CFormInput"
+                    name="password"
+                    type="password"
+                    id="password"
+                    placeholder="password"
+                    :invalid="!!errors?.password"
+                    :feedback-invalid="errors?.password"
+                  />
+                  <CFormLabel for="password"> Password </CFormLabel>
+                </CFormFloating>
+
+                <CFormFloating class="my-4">
+                  <Field
+                    as="CFormInput"
+                    name="confirm_password"
+                    type="password"
+                    id="confirm_password"
+                    placeholder="Confirm password"
+                    :invalid="!!errors?.confirm_password"
+                    :feedback-invalid="errors?.confirm_password"
+                  />
+                  <CFormLabel for="confirm_password">
+                    Confirm password
+                  </CFormLabel>
+                </CFormFloating>
+
+                <div class="d-grid gap-2 col-4 mx-auto my-2">
+                  <CButton
+                    class="px-4"
+                    type="submit"
+                    color="light"
+                    size="lg"
+                    :disabled="isSubmiting || Object.keys(errors).length > 0"
+                  >
+                    <CSpinner
+                      v-if="isSubmiting"
+                      component="span"
+                      size="sm"
+                      aria-hidden="true"
+                    />
+                    <span v-else>REGISTER</span>
+                  </CButton>
+                </div>
+                <div class="d-grid gap-2 col-8 mx-auto my-2">
+                  <CButton class="px-8" color="primary" size="lg">
+                    <span>SIGN IN WITH FACEBOOK</span>
+                  </CButton>
+                </div>
+                <div class="d-grid gap-2 col-8 mx-auto my-2">
+                  <CButton class="px-8" color="danger" size="lg">
+                    <span>SIGN IN WITH EMAIL</span>
+                  </CButton>
+                </div>
+                <div class="d-grid gap-2 col-4 mx-auto my-2">
+                  <CButton
+                    class="px-4"
+                    color="light"
+                    size="lg"
+                    @click="() => $router.push({ name: 'Login' })"
+                  >
+                    <span>SIGN IN</span>
+                  </CButton>
+                </div>
+              </Form>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </div>
+</template>
+
+<script>
+import { Form, Field } from 'vee-validate'
+import * as yup from 'yup'
+
+export default {
+  name: 'Register',
+  components: {
+    Form,
+    Field,
+  },
+  data() {
+    const schema = yup.object({
+      username: yup.string().required('User name is required'),
+      password: yup
+        .string()
+        .required('password is required')
+        .min(8, 'password is required'),
+      confirm_password: yup
+        .string()
+        .required('confirm password is required')
+        // .oneOf([yup.ref('password'), null], "Passwords don't match!")
+        .min(8, 'password is required'),
+    })
+
+    return {
+      isSubmiting: false,
+      schema,
+    }
+  },
+  methods: {
+    async onSubmit(values) {
+      this.isSubmiting = true
+
+      const body = {
+        username: values.username,
+        password: values.password,
+        confirm_password: values.confirm_password,
+      }
+
+      try {
+        // const response = await login(body)
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            console.log(body)
+            resolve()
+          }, 500),
+        )
+        // if (response?.data) {
+        //   setAccessToken(response.data.access_token, {
+        //     expires: new Date(Date.now() + response.data.expires_in * 1000),
+        //   })
+
+        //   this.$router.replace({ name: 'Home' })
+        // }
+      } finally {
+        this.isSubmiting = false
+      }
+    },
+  },
+}
+</script>
